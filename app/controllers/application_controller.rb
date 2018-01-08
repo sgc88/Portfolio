651da5e9cdb3900c847b70fbce47052dd5dc3478
sgc88 <sehribancheng@yahoo.com>
2018-01-08 00:22:52 -0800

    trying some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def pdf
-    send_file(
-      "#{Rails.root}/public/cv.pdf",
-      filename: "cv.pdf",
-      type: "resume/pdf"
-    )
-  end
+
 end
